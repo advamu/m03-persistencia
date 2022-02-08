a6ad2e7880651cafa8f9c24b0049a63e0ab8b871
Adrian Valeros <a2102999@institutmontilivi.cat>
2022-02-08 18:16:06 +0000

    fet get i post

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const { disconnect } = require("mongoose");
 const { getConnection } = require("./dbConnection");
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser')
+
+app.use(express.json())
 
 const User = require("./model/user.model");
 
@@ -13,6 +17,27 @@ app.get("/", async (req, res) => {
   return res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.post("/user", async (req, res) => {
+console.log('req.body')
+
+const user = await User.create({
+  username: req.body.username,
+  password: req.body.password,
+})
+
+return res.send("")
+})
+
+app.listen(port, async () => {
+  await getConnection();
   console.log(`Example app listening on port ${port}`);
+});
+
+process.on("SIGINT", function () {
+  disconnectDB().then(() => {
+    server.close(function () {
+      console.log("closed connection");
+      process.exit(0);
+    });
+  });
 });
